17a6eedf622ab064b9150a062bdfc037ae5a3703
Jaymin Dattani <work.jdattani@gmail.com>
2026-07-25 13:07:46 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MTSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
